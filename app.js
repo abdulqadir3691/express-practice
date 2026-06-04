8bbaf0ddd5c08e36100744674e929fc15ac676af
unknown <abdulqadir3691@gmail.com>
2026-06-04 17:42:58 +0500

    Initial commit

--- app.js
+++ app.js
@@ -4,33 +4,33 @@ const port = 8000;
 const path = require('path');
 
 // using static files in express as middleware to get whole frontend folder and its content
-// app.use(express.static('./frontend'));
+app.use(express.static('./frontend'));
 
-// app.get('/', (req, res) =>{
-//     res.status(200).sendFile(path.resolve(__dirname, './frontend/index.html'))
-// });
+app.get('/', (req, res) =>{
+    res.status(200).sendFile(path.resolve(__dirname, './frontend/index.html'))
+});
 
-// app.get('/', (req, res) =>{
-//     res.send("<h1>This is the home page</h1>")
-// })
+app.get('/', (req, res) =>{
+    res.send("<h1>This is the home page</h1>")
+})
 
-// app.get('/about', (req, res) =>{
-//     res.send("<h2>This is the about page</h2>")
-// })
-// // un authorized route and page not found route using regex * symbol
-// app.get(/(.*)/, (req, res) =>{
-//     res.send("<h3>404 - Page not found</h3>")
-// })
+app.get('/about', (req, res) =>{
+    res.send("<h2>This is the about page</h2>")
+})
+// un authorized route and page not found route using regex * symbol
+app.get(/(.*)/, (req, res) =>{
+    res.send("<h3>404 - Page not found</h3>")
+})
 
 // making an api in backend and sending data to frontend
 
-app.get ('/', (req, res) =>{
-  return res.json({
-    name : "John Doe",
-    age : 30,
-    city : "New York"
-  })  
-})
+// app.get ('/', (req, res) =>{
+//   return res.json({
+//     name : "John Doe",
+//     age : 30,
+//     city : "New York"
+//   })  
+// })
 
 
 app.listen(port, () =>{
